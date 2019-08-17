ee143b8d330e8498eb35c1b466dc304f6732eb8f
Anna Greaves <gilhespy.anna@gmail.com>
2019-08-17 13:53:27 +0200

    Test flask route working.

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ from flask import Flask, redirect, render_template, request, url_for
 # create instance of flask and assign it to "app"
 app = Flask(__name__)
 
+# Home page
+@app.route('/')
+def home_page():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':

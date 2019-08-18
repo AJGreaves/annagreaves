e7804d55754369417b55ea193038cb869bae2665
Anna Greaves <gilhespy.anna@gmail.com>
2019-08-18 19:04:54 +0200

    Add portfolio links to navigation.

--- app.py
+++ app.py
@@ -7,33 +7,33 @@ app = Flask(__name__)
 # Home page
 @app.route('/')
 def home_page():
-    return render_template(
-        'index.html',
-        active='home',
-        title="Home")
+        return render_template(
+                'index.html',
+                active='home',
+                title="Home")
 
 # Contact page
 @app.route('/contact')
 def contact_page():
-    return render_template(
-        'contact.html',
-        active='contact',
-        title="Contact")
+        return render_template(
+                'contact.html',
+                active='contact',
+                title="Contact")
 
 # Portfolio page
 @app.route('/portfolio')
 def portfolio_page():
-    return render_template(
-        'portfolio.html',
-        active='portfolio',
-        title="Portfolio")
+        return render_template(
+                'portfolio.html',
+                active='portfolio',
+                title="Portfolio")
 
 @app.route('/portfolio/familyhub')
 def portfolio_familyhub():
-    return render_template(
-    'projects/familyhub.html',
-    active="portfolio",
-    title="Portfolio | Family Hub")
+        return render_template(
+                'projects/familyhub.html',
+                active="portfolio",
+                title="Portfolio | Family Hub")
 
 if __name__ == '__main__':
-    app.run(host=os.getenv('IP'), port=os.getenv('PORT'), debug=True)
+        app.run(host=os.getenv('IP'), port=os.getenv('PORT'), debug=True)
